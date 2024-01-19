cc92f858c2d761be230f3364bbd4cce2d2b65726
Alex Shkor <alex.shkor@gmail.com>
2024-01-19 11:55:55 +0100

    final deployment fix

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,7 +1,15 @@
 const OwnableERC20Token = artifacts.require("OwnableERC20Token");
 const Processor = artifacts.require("Processor");
 
-module.exports = function(deployer) {
-  deployer.deploy(OwnableERC20Token);
-  deployer.deploy(Processor);
+module.exports = async function(deployer) {
+
+  await deployer.deploy(OwnableERC20Token, "oUSD", "oUSD", 0);
+  const tokenInstance = await OwnableERC20Token.deployed();
+
+  // Deploy Processor
+  await deployer.deploy(Processor, "0xc4bF5CbDaBE595361438F8c6a187bDc330539c60", tokenInstance.address);
+  const processorInstance = await Processor.deployed();
+
+  // Set the owner of the ERC20 token to the Processor's address
+  await tokenInstance.transferOwnership(processorInstance.address);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// require('dotenv').config();
-// const mnemonic = process.env["MNEMONIC"];
+require('dotenv').config();
+const mnemonic = process.env["MNEMONIC"];
 // const infuraProjectId = process.env["INFURA_PROJECT_ID"];
  
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   /**
@@ -42,17 +42,22 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
     //
-    // goerli: {
-    //   provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraProjectId}`),
-    //   network_id: 5,       // Goerli's id
-    //   chain_id: 5
-    // }
+    goerli: {
+      provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraProjectId}`),
+      network_id: 5,       // Goerli's id
+      chain_id: 5
+    },
+    sepolia: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc.sepolia.org/`),
+      network_id: 11155111,       // Goerli's id
+      chain_id: 6
+    }
   },
 
   // Set default mocha options here, use special reporters etc.
