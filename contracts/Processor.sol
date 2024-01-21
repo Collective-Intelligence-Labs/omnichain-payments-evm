7335dcefc6a7c5e78d9e97d59d4b38dc14b02b3d
Alex Shkor <alex.shkor@gmail.com>
2024-01-21 09:11:40 +0100

    Operations introduced

--- contracts/Processor.sol
+++ contracts/Processor.sol
@@ -10,21 +10,29 @@ contract Processor {
     IERC20 public usdtToken;
     ERC20Permit public permitToken;
 
+    mapping(uint => function (AssetTransfer memory, Operataion memory) internal returns(bool)) handlers;
+
     uint public constant TRANSFER = 1;
     uint public constant DEPOSIT = 2;
     uint public constant WITHDRAW = 3;
 
-    struct TransferData {
-        bytes sign;
-        bytes data;
-    }
-
     struct AssetTransfer {
-        uint256 cmd_id;
         uint256 cmd_type;
         uint256 amount;
         address from;
         address to;
+    }
+
+    struct Operataion {
+        bytes[] commands;
+        bytes[] signatures;
+        Metadata metadata;
+    }
+
+    struct Metadata {
+        uint256 id;
+        address payee;
+        address router;
         uint256 fee;
         uint256 deadline;
     }
@@ -32,7 +40,7 @@ contract Processor {
     mapping(uint256 => uint256) private _nonces; 
 
     event TokensProcessed(address indexed sender, uint256 usdtAmount, uint256 processedTokenAmount, uint256 fee);
-    event TokensTransfered(address indexed sender, uint256 usdtAmount, uint256 processedTokenAmount, uint256 fee);
+    event TokensTransfered(address indexed sender, uint256 usdtAmount, uint256 processedTokenAmount);
     event TokensWithdrawn(address indexed recipient, uint256 processedTokenAmount);
     event TokenDeposited(address indexed sender, address recipent, uint256 processedTokenAmount);
 
@@ -40,97 +48,107 @@ contract Processor {
         processedToken = OwnableERC20Token(_internalTokenAddress);
         usdtToken = IERC20(_targetTokenAddress);
         permitToken = ERC20Permit(_targetTokenAddress);
+        handlers[TRANSFER] = transfer;
+        handlers[DEPOSIT] = deposit;
+        handlers[WITHDRAW] = withdraw;
     }
 
-    function processCmds(bytes[] calldata jsonList) external {
-        for (uint256 i = 0; i < jsonList.length; i++) {
-            TransferData memory transferData = abi.decode(jsonList[i], (TransferData));
-            AssetTransfer memory data = abi.decode(transferData.data, (AssetTransfer));
-            (uint8 v, bytes32 r, bytes32 s) = splitSignatureWithSlicing(transferData.sign);
-            if (data.amount == 0) {
-                // Skip iteration if the transfer amount is zero
-                continue;
-            }
-
-            if (_nonces[data.cmd_id] == 0)
-            {
-                continue;
-            }
-
-            if (data.deadline < block.timestamp)
-            {
-                continue;
-            }
-
-            // Ensure that the sender has enough balance to perform the transfer
-            if (processedToken.balanceOf(data.from) < data.amount) {
+    function transfer(AssetTransfer memory data, Operataion memory op) internal returns(bool) {
+        if (processedToken.balanceOf(data.from) < data.amount) {
                 // Skip iteration if the sender has insufficient balance
-                continue;
-            }
+                return false;
+        }
 
-            // Validate the signature
-            if ((data.cmd_type != DEPOSIT) && 
-                !validateSignature(data.from, v, r, s, calculateHash(transferData.data)))
-            {   // Skip iteration if the signature is invalid
-                continue;
-            }
+        processedToken.transferFrom(data.from, data.to, data.amount);
+        emit TokensTransfered(msg.sender, data.amount, data.amount);
+        return true;
+    }
 
-            if (data.cmd_type == TRANSFER)
-            {
-                // Perform the transfer
-                processedToken.transferFrom(data.from, data.to, data.amount);
-                emit TokensTransfered(msg.sender, data.amount, data.amount, data.fee);
-            }
-
-            if (data.cmd_type == DEPOSIT)
-            {
-                permitToken.permit(data.from, address(this), data.amount, data.deadline, v, r, s);
+    function deposit(AssetTransfer memory data, Operataion memory op) internal returns(bool) {
+        (uint8 v, bytes32 r, bytes32 s) = splitSignatureWithSlicing(op.signatures[0]);
+        permitToken.permit(data.from, address(this), data.amount, op.metadata.deadline, v, r, s);
                 if (!usdtToken.transferFrom(data.from, address(this), data.amount))
                 {
-                    continue;
+                    return false;
                 }
                 processedToken.mint(data.to, data.amount);
                 emit TokenDeposited(msg.sender, data.to, data.amount);
-            }
+        return true;
+    }
 
-            if (data.cmd_type == WITHDRAW)
-            {
-                if (processedToken.balanceOf(data.from) > data.amount)
+    function withdraw(AssetTransfer memory data, Operataion memory op) internal returns(bool) {
+         if (processedToken.balanceOf(data.from) > data.amount)
                 {
                     processedToken.burn(msg.sender, data.amount);
                     usdtToken.transfer(msg.sender, data.amount);
                     emit TokensWithdrawn(msg.sender, data.amount);
                 }
-            }
+        return true;
+    }
 
-            // Perform the transfer fee
-            processedToken.transferFrom(data.from, msg.sender, data.fee);
-            _nonces[data.cmd_id] = block.timestamp;
-            emit TokensProcessed(msg.sender, data.amount, data.amount, data.fee);
+    function processCmds(bytes[] calldata ops) external returns (uint256[] memory){
+        uint256[] memory ids = new uint256[](ops.length);
+        for (uint256 i = 0; i < ops.length; i++) {
+            Operataion memory op = abi.decode(ops[i], (Operataion));
+            if (processOperation(op))
+            {
+                ids[i] = op.metadata.id;
+            }
         }
+        return ids;
+    }
+
+    function processOperation(Operataion memory op) internal returns (bool) {
+        address[] memory signers = extractSigners(op);
+        if (_nonces[op.metadata.id] != 0) {
+            return false;
+        }
+        if (op.metadata.deadline < block.timestamp)
+        {
+            return false;
+        }
+        for (uint256 i = 0; i < op.commands.length; i++) {
+            processCommand(op.commands[i], op.metadata, signers, op);
+        }
+        _nonces[op.metadata.id] = block.timestamp;
+        if (signers[0] != op.metadata.payee)
+        {
+            return false;
+        }
+        processedToken.transferFrom(op.metadata.payee, op.metadata.router, op.metadata.fee);
+        return true;
+    }
+
+    function processCommand(bytes memory cmdBytes, Metadata memory metadata, address[] memory signers, Operataion memory op) internal returns (bool)
+    {   
+         AssetTransfer memory cmd = abi.decode(cmdBytes, (AssetTransfer));
+         function (AssetTransfer memory, Operataion memory) internal returns(bool)  handler = handlers[cmd.cmd_type];
+         return handler(cmd, op);
     }
 
     function calculateHash(bytes memory data) internal pure returns (bytes32) {
         return keccak256(data);
     }
 
-    function splitSignatureWithSlicing(bytes memory signature) public pure returns (uint8 v, bytes32 r, bytes32 s){
-        // Divide the signature in r, s and v variables with inline assembly.
-        assembly {
-        r := mload(add(signature, 0x20))
-        s := mload(add(signature, 0x40))
-        v := byte(0, mload(add(signature, 0x60)))
+    function calculateOperaionHash(Operataion memory op) internal pure returns (bytes32) {
+        return keccak256(abi.encode(op.commands, op.metadata));
+    }
+
+    function extractSigners(Operataion memory op) internal pure returns (address[] memory) {
+        bytes32 dataHash = calculateOperaionHash(op);
+        address[] memory recovered = new address[](op.signatures.length);
+        for (uint i = 0; i < op.signatures.length; i++) {
+            (uint8 v, bytes32 r, bytes32 s) = splitSignatureWithSlicing(op.signatures[i]);
+            address recoveredAddress = ecrecover(dataHash, v, r, s);
+            recovered[i] = recoveredAddress;
         }
-
+        return recovered;
     }
 
-    /*
-
-    function calculateDigest(AssetTransfer memory data) internal pure returns (bytes32) {
-        return keccak256(abi.encodePacked(data.cmd_id, data.cmd_type, data.amount, data.from, data.to, data.fee, data.deadline));
+    function splitSignatureWithSlicing(bytes memory signature) public pure returns (uint8 v, bytes32 r, bytes32 s){
+            (r, s) = abi.decode(signature, (bytes32, bytes32));
+            v = uint8(signature[64]);
     }
-
-    */
     
     function validateSignature(address from, uint8 v, bytes32 r, bytes32 s, bytes32 dataHash) internal pure returns (bool) {
         address recoveredAddress = ecrecover(dataHash, v, r, s);
